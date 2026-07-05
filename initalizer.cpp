938388e0d9fb616d1b9b989210bec031c249b105
MadhavBuilds <madhavradhey34@gmail.com>
2026-07-05 21:13:57 +0530

    initalization array

--- initalizer.cpp
+++ initalizer.cpp
@@ -1,1 +1,8 @@
-// so this 
+// so this is the initalization on the arr 
+#include<iostream>
+using namespace std;
+int main (){
+    int arr [5] = {3,5,6,4,8};
+    cout<<arr[3]<<endl;
+    return 0;
+}
